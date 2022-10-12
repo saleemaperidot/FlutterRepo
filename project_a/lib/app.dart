aadd857dcaaa6e3402347a65eebb47cc34cc38b0
saleema np <saleemaperidot@gmail.com>
2022-10-12 22:55:20 +0530

    Learning Widgets

---
+++ project_a/lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: "My APP A",
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Text("MyAppthi"),
+          ),
+        ),
+      ),
+    );
+    throw UnimplementedError();
+  }
+}
---
+++ project_a/lib/main.dart
@@ -0,0 +1,11 @@
+//import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import './app.dart';
+
+void main() {
+//defining material app(Android)
+//By calling object.We can also use by contructor
+  // ignore: prefer_const_constructors
+
+  runApp(App());
+}
